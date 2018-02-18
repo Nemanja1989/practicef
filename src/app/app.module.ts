edcd89b1d653dbaf8872b559b827f237e62dcabe
Nemanja Bojanic <nemanja.bojanic@pool4tool.com>
2018-02-18 12:13:24 +0100

    master layout, navbar and nice table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TeamListComponent } from './components/team-list/team-list.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TeamListComponent
+    TeamListComponent,
+    LayoutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
