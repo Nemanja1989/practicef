0428b1914ecddcc1004dcba7097a8efba7acba87
Nemanja Bojanic <nemanja.bojanic@pool4tool.com>
2018-02-18 11:54:17 +0100

    master showing fake data on page

--- src/app/components/team-list/team-list.component.ts
+++ src/app/components/team-list/team-list.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-team-list',
-  templateUrl: './team-list.component.html',
-  styleUrls: ['./team-list.component.css']
+    selector: 'app-team-list',
+    templateUrl: './team-list.component.html',
+    styleUrls: ['./team-list.component.css']
 })
 export class TeamListComponent implements OnInit {
 
-  constructor() { }
+    teams;
 
-  ngOnInit() {
-  }
+    constructor() {
+        this.teams = [
+            {
+                name: 'John',
+                city: 'Futog',
+                email: 'john@example.com'
+            },
+            {
+                name: 'Daniel',
+                city: 'Veternik',
+                email: 'daniel@example.com'
+            },
+            {
+                name: 'Martin',
+                city: 'Vukojebina',
+                email: 'martin@example.com'
+            },
+            {
+                name: 'Martin',
+                city: 'Begec',
+                email: 'martin@example.com'
+            },
+            {
+                name: 'Nemanja',
+                city: 'Novi Sad',
+                email: 'nemanjabojanic@example.com'
+            }
+        ];
+
+    }
+
+    ngOnInit() {
+    }
 
 }
